68495358dde42d6435276ddc61459964d42566b5
Innocloneus <167940727+Innocloneus@users.noreply.github.com>
2024-07-14 19:21:52 +0700

    Made bad

--- src/types.h
+++ src/types.h
@@ -310,9 +310,15 @@ constexpr CastlingRights operator&(Color c, CastlingRights cr) {
     return CastlingRights((c == WHITE ? WHITE_CASTLING : BLACK_CASTLING) & cr);
 }
 
-constexpr Value mate_in(int ply) { return VALUE_MATE - ply; }
+constexpr Value mate_in(int ply) {
+  Value zero = Value(0);
+  return zero - (VALUE_MATE - ply);
+}
 
-constexpr Value mated_in(int ply) { return -VALUE_MATE + ply; }
+constexpr Value mated_in(int ply) {
+  Value zero = Value(0);
+  return zero - (-VALUE_MATE + ply);
+}
 
 constexpr Square make_square(File f, Rank r) { return Square((r << 3) + f); }
 
